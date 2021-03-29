6fc8ff42a52dfbde216a1459629619128d452043
bsuraj23 <bsuraj23@gmail.com>
2021-03-29 21:19:56 +0530

    adding into main on Monday Happy

---
+++ package2/Sample.java
@@ -0,0 +1,31 @@
+package package1;
+
+import java.lang.*;
+
+class Other {
+
+    int a;
+    String str = "Karthik";
+    byte b;
+    char c = 'a';
+
+}
+
+public class Sample23 {
+
+public static void main(String args[]){
+
+System.out.println("Secodnime");
+    Other obj = new Other();
+    System.out.println(obj.a);
+    System.out.println(obj.);
+    System.out.println(obj.a);
+    System.out.println(obj.a);
+    System.out.println(obj.a);
+
+                                    }
+}
+
+
+
+
